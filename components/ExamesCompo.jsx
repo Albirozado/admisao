66a70025966b5ed9aeaf5d8e3610c4c48159ea46
Albirozado <albirosaldoalfredo12@gmail.com>
2023-08-24 16:12:04 -0700

    marginExameCompo

--- components/ExamesCompo.jsx
+++ components/ExamesCompo.jsx
@@ -5,7 +5,7 @@ import up from "../public/instituicoes/up.png"
 import icm from "../public/instituicoes/icm.png"
 export default function ExamesCompo(){
     return(
-        <section className="mb-5 sm:mx-5 mx-3 sm:mt-0 mt-44 ">
+        <section className="mb-5 sm:mx-5 mx-3 sm:mt-0 mt-44 exameCompo">
             <h1 className="text-center text-[#160E04] font-bold sm:base text-3xl">baixe exame de <span className="text-[#FF931E]">todas as instituicoes</span> publicas </h1>
             <div className="container grid sm:grid-cols-4 grid-cols-2 gap-4">
                 <div className=" text-[#fff] shadow-md border-solid border-1 border-[#ccc] py-3 rounded-lg flex justify-center">
