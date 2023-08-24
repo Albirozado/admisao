bfd2741c7b8fee6eda57fc770b380de631000059
Albirozado <albirosaldoalfredo12@gmail.com>
2023-08-24 12:49:12 +0200

    comoja

--- components/HeaderCompo.jsx
+++ components/HeaderCompo.jsx
@@ -4,10 +4,10 @@ export default function HeaderCompo(){
     return(
         <section>
         <div className="header">
-          <div className="textContetent header_content">
+          <div className="textContetent header_content ">
             <span className="stokWeb block plantaforma sm:text-lg text-base font-bold text-[#FF931E]">Plantaforma de Educacional</span>
-            <h1 className="font-extrabold sm:text-4xl text-xl text-[#160E04] leading-6">Tenha acesso a uma <span className="text-[#FF931E]">dezena de material</span> de aprendizagem e de avaliacao.</h1>
-            <p className="stokWeb sm:text-lg sm:leading-6 leading-5 font-normal mb-1 text-[#160E04] text-base">aqui voce encontrara uma gama diversificada de recursos para enriquecer sua jornada educacional</p>
+            <h1 className="font-extrabold sm:text-4xl text-3xl text-[#160E04]  leading-8 ">Tenha acesso a uma <span className="text-[#FF931E]">dezena de material</span> de aprendizagem e de avaliacao.</h1>
+            <p className="sm:text-lg sm:leading-6 leading-4 font-medium mb-1 text-[#333] text-sm">aqui voce encontrara uma gama diversificada de recursos para enriquecer sua jornada educacional</p>
             <button className="bg-[#FF931E] font-semibold text-white py-1 px-2 rounded-sm ">Explorar</button>
           </div>  
 
