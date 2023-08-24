77bdaf72720feed4c2e535b5648863fa083ff086
Albirozado <albirosaldoalfredo12@gmail.com>
2023-08-24 12:59:52 +0200

    mode123

--- components/HeaderCompo.jsx
+++ components/HeaderCompo.jsx
@@ -8,7 +8,7 @@ export default function HeaderCompo(){
             <span className="stokWeb block plantaforma sm:text-lg text-base font-bold text-[#FF931E]">Plantaforma de Educacional</span>
             <h1 className="font-extrabold sm:text-4xl text-3xl text-[#160E04]  leading-8 ">Tenha acesso a uma <span className="text-[#FF931E]">dezena de material</span> de aprendizagem e de avaliacao.</h1>
             <p className="sm:text-lg sm:leading-6 leading-4 font-medium mb-1 text-[#333] text-sm">aqui voce encontrara uma gama diversificada de recursos para enriquecer sua jornada educacional</p>
-            <button className="bg-[#FF931E] font-semibold text-white py-1 px-2 rounded-sm ">Explorar</button>
+            <button className="bg-[#FF931E] font-semibold text-white py-1 px-2 mb-3 rounded-sm ">Explorar</button>
           </div>  
 
           <div className="girlImage header_content">
