6695293f586ecd3e9167bf8475f349fae3a0ff88
Albirozado <albirosaldoalfredo12@gmail.com>
2023-08-28 06:51:26 -0700

    hover

--- client/NavigatioBar.jsx
+++ client/NavigatioBar.jsx
@@ -8,26 +8,25 @@ function Navigatiobar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
   
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto"> {/* Use "ms-auto" class to align items to the right */}
             <NavDropdown title="Exames" id="basic-nav-dropdown" style={{fontWeight: "600"}}>
               <div style={{backgroundColor: "#f8f9fa"}}>
-              <NavDropdown.Item href="/exames/uem/portugues">Universidade E. Mondlane</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item href="/exames/uem/portugues" className='no-hover' style={{marginBottom: "0.5rem"}}>Universidade E. Mondlane</NavDropdown.Item>
+              <NavDropdown.Item href="/exames/up/portugues" style={{marginBottom: "0.5rem"}}>
                 Universidade Pedagogica
               </NavDropdown.Item>
-              <NavDropdown.Item href="/exames/up/portugues">Universidade J. Chissano</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/exames/uem/portugues" >
+              <NavDropdown.Item href="/exames/up/portugues" style={{marginBottom: "0.5rem"}}>Universidade J. Chissano</NavDropdown.Item>
+              <NavDropdown.Item href="/exames/uem/portugues" style={{marginBottom: "0.5rem"}}>
                 Universidade J. Chissano
               </NavDropdown.Item>
 
               </div>
             </NavDropdown>
-            <Nav.Link href="/material" style={{fontWeight: "600"}}>Material & Livros</Nav.Link>
+            <Nav.Link href="/material" style={{fontWeight: "600"}} >Material & Livros</Nav.Link>
             <Nav.Link href="/blog" style={{fontWeight: "600"}}>Blog</Nav.Link>
 
           </Nav>
