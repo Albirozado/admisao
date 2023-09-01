717145fd82e954d4edc0e9f5153986f4117a5e7b
Albirozado <albirosaldoalfredo12@gmail.com>
2023-09-01 18:21:47 +0200

    fontBold

--- components/materialInfantil/banner.jsx
+++ components/materialInfantil/banner.jsx
@@ -2,7 +2,7 @@ import styles from "../../client/MaterialStyle.module.css"
 export default function BannerMaterial(){
     return(
         <header className={styles.Banner}>
-            <h1 className={`text-center text-[#fff] font-bold sm:base text-3xl${styles.BannerTittle}`}>Aqui voce encontrara todo material para o apredendizado infantil de alta qualidadesx</h1>
+            <h1 className="text-center text-[#fff] font-bold sm:base text-3xl ">Aqui voce encontrara todo material para o apredendizado infantil de alta qualidadesx</h1>
 
         </header>
     )
