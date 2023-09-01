e68ff1541ffcfd427a2f7be8faa13a4c56734299
Albirozado <albirosaldoalfredo12@gmail.com>
2023-09-01 17:58:01 +0200

    qualidade

--- components/materialInfantil/banner.jsx
+++ components/materialInfantil/banner.jsx
@@ -2,7 +2,7 @@ import styles from "../../client/MaterialStyle.module.css"
 export default function BannerMaterial(){
     return(
         <header className={styles.Banner}>
-            <h1 className={`className="text-center font-bold sm:base text-3xl"${styles.BannerTittle}`}>Aqui voce encontrara todo material para o apredendizado infantil de alta qualidade</h1>
+            <h1 className={`className="text-center font-bold sm:base text-3xl"${styles.BannerTittle}`}>Aqui voce encontrara todo material para o apredendizado infantil de alta qualidadesx</h1>
 
         </header>
     )
